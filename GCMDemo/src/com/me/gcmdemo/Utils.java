ace65388c459eeb45e6a4aa664e7d004e20772d4
Ashutosh Mishra <ashutoshm.87@gmail.com>
2013-12-18 13:22:03 +0530

    New GCM Notification

---
+++ GCMDemo/src/com/me/gcmdemo/Utils.java
@@ -0,0 +1,11 @@
+package com.me.gcmdemo;
+
+import android.app.Application;
+
+public class Utils extends Application{
+
+	public static String GCMSenderId = "834854426974";
+	public static boolean notificationReceived;
+	public static String notiTitle="",notiType="",notiMsg="",notiUrl="",registrationId = "";
+	
+}
